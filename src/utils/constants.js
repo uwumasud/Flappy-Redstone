e06aa72c2dc3c0afe6bfdaf4b6b34c4db511be0a
uwumasud <fflover0338@gmail.com>
2025-10-14 00:21:09 +0600

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,28 @@
+// Auto-generated to match your uploaded sprites
 export const PLAYERS = [
-  ["assets/sprites/redbird-upflap.png","assets/sprites/redbird-midflap.png","assets/sprites/redbird-downflap.png"],
-  ["assets/sprites/bluebird-upflap.png","assets/sprites/bluebird-midflap.png","assets/sprites/bluebird-downflap.png"],
-  ["assets/sprites/yellowbird-upflap.png","assets/sprites/yellowbird-midflap.png","assets/sprites/yellowbird-downflap.png"]
+  [
+    "assets/sprites/bluebird-upflap.png",
+    "assets/sprites/bluebird-midflap.png",
+    "assets/sprites/bluebird-downflap.png"
+  ],
+  [
+    "assets/sprites/redbird-upflap.png",
+    "assets/sprites/redbird-midflap.png",
+    "assets/sprites/redbird-downflap.png"
+  ],
+  [
+    "assets/sprites/yellowbird-upflap.png",
+    "assets/sprites/yellowbird-midflap.png",
+    "assets/sprites/yellowbird-downflap.png"
+  ]
 ];
-export const BACKGROUNDS = ["assets/sprites/background-day.png","assets/sprites/background-night.png"];
-export const PIPES = ["assets/sprites/pipe-green.png","assets/sprites/pipe-red.png"];
+
+export const BACKGROUNDS = [
+  "assets/sprites/background-day.png",
+  "assets/sprites/background-night.png"
+];
+
+export const PIPES = [
+  "assets/sprites/pipe-green.png",
+  "assets/sprites/pipe-red.png"
+];
