5897342dbebbe47ae52887d91ba6223921e0a339
uwumasud <fflover0338@gmail.com>
2025-10-14 21:54:58 +0600

    Update floor.js

--- src/entities/floor.js
+++ src/entities/floor.js
@@ -1,28 +1,58 @@
 import { Entity } from './entity.js';
-const FLOOR_SCROLL_SPEED = 1.6; // close to |PIPE_SPEED|
 
 export class Floor extends Entity {
   constructor(config){
-    super(config, config.images.base || null, 0, config.window.vh);
-    this.vel_x = FLOOR_SCROLL_SPEED;
-    this.w = (this.image?.width)  || (config.window.w + 48);
-    this.h = (this.image?.height) || (config.window.h - this.y);
-    this.x_extra = Math.max(1, this.w - config.window.w);
+    super(config, config.images.base, 0, 0);
+
+    this.vel_x = 2.4;
+
+    // before image loads, use sane defaults
+    this._imgW = this.image?.width  || config.window.w;
+    this.h     = this.image?.height || 112;
+    this.y     = config.window.h - this.h;
+
+    this._tileW = this._imgW || config.window.w;
+    this.x = 0;
+
+    // share ground to everyone (player clamp, collisions, etc.)
+    this.config.groundY = this.y;
   }
-  stop(){ this.vel_x = 0; }
-  update(dt=1){ this.x = -((-this.x + this.vel_x * dt) % this.x_extra); }
-  tick(){
-    if (this.image && (this.w !== this.image.width || this.h !== this.image.height)) {
-      this.w = this.image.width  || this.w;
-      this.h = this.image.height || this.h;
-      this.x_extra = Math.max(1, this.w - this.config.window.w);
+
+  _syncDimsIfReady(){
+    if (this.image && this.image.width && this.image.height){
+      if (this.h !== this.image.height){
+        this.h = this.image.height;
+        this.y = this.config.window.h - this.h;
+      }
+      this._imgW  = this.image.width;
+      this._tileW = this._imgW;
     }
+  }
+
+  update(dt=1){
+    this._syncDimsIfReady();
+    // keep groundY up-to-date every frame
+    this.config.groundY = this.y;
+
+    this.x = (this.x - this.vel_x * dt) % this._tileW;
+  }
+
+  tick(dt=1){
+    this.update(dt);
     const ctx = this.config.ctx;
-    if (this.image?.width){
-      ctx.drawImage(this.image, this.x, this.y);
-      ctx.drawImage(this.image, this.x + this.w, this.y);
+
+    if (this.image && this.image.width){
+      let drawX = this.x;
+      while (drawX > 0) drawX -= this._tileW;
+      for (; drawX < this.config.window.w; drawX += this._tileW){
+        ctx.drawImage(this.image, drawX, this.y);
+      }
     } else {
-      ctx.fillStyle = '#283246'; ctx.fillRect(0, this.y, this.config.window.w, this.config.window.h - this.y);
+      // fallback ground so there’s never a blank
+      ctx.fillStyle = '#E9DF9A';
+      ctx.fillRect(0, this.y, this.config.window.w, this.h);
+      ctx.fillStyle = '#D36A67';
+      ctx.fillRect(0, this.y - 6, this.config.window.w, 6);
     }
   }
 }
